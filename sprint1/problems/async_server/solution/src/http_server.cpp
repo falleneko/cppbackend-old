59998b09f1de1f17481df262569f634d0f910b90
Петр Никифоров <peternimail@gmail.com>
2026-09-09 14:48:23 +0300

    s1t4

---
+++ sprint1/problems/async_server/solution/src/http_server.cpp
@@ -0,0 +1,24 @@
+#include "http_server.h"
+
+#include <boost/asio/dispatch.hpp>
+#include <iostream>
+
+namespace http_server {
+    const std::unordered_map<std::string_view, HttpMethodList> MethodToString = {
+        {"GET"sv, HttpMethodList::GET},
+        {"POST"sv, HttpMethodList::POST},
+        {"HEAD"sv, HttpMethodList::HEAD},
+    };
+
+    void ReportError(beast::error_code ec, std::string_view what) {
+        std::cerr << what << ": "sv << ec.message() << std::endl;
+    }
+
+    void SessionBase::Run() {
+        // Вызываем метод Read, используя executor объекта stream_.
+        // Таким образом вся работа со stream_ будет выполняться, используя его executor
+        net::dispatch(stream_.get_executor(),
+                    beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
+    }
+
+}  // namespace http_server
